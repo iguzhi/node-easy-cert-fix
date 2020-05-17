19aa197b9087404760836d98c3d7d7e3f6f27b13
weilee <383523223@qq.com>
2020-05-18 00:35:18 +0800

    fix

--- dist/certGenerator.js
+++ dist/certGenerator.js
@@ -1,21 +1,18 @@
-'use strict'
+'use strict';
 
-const forge = require('node-forge');
-const Util = require('./util');
+var forge = require('node-forge');
+var Util = require('./util');
 
-let defaultAttrs = [
-  { name: 'countryName', value: 'CN' },
-  { name: 'organizationName', value: 'EasyCert' },
-  { shortName: 'ST', value: 'SH' },
-  { shortName: 'OU', value: 'EasyCert SSL' }
-];
+var defaultAttrs = [{ name: 'countryName', value: 'CN' }, { name: 'organizationName', value: 'EasyCert' }, { shortName: 'ST', value: 'SH' }, { shortName: 'OU', value: 'EasyCert SSL' }];
 
 /**
 * different domain format needs different SAN
 *
 */
-function getExtensionSAN(domain = '') {
-  const isIpDomain = Util.isIpDomain(domain);
+function getExtensionSAN() {
+  var domain = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+  var isIpDomain = Util.isIpDomain(domain);
   if (isIpDomain) {
     return {
       name: 'subjectAltName',
@@ -30,42 +27,39 @@ function getExtensionSAN(domain = '') {
 }
 
 function getKeysAndCert(serialNumber) {
-  const keys = forge.pki.rsa.generateKeyPair(2048);
-  const cert = forge.pki.createCertificate();
+  var keys = forge.pki.rsa.generateKeyPair(2048);
+  var cert = forge.pki.createCertificate();
   cert.publicKey = keys.publicKey;
-  cert.serialNumber = serialNumber || (Math.floor(Math.random() * 100000) + '');
+  cert.serialNumber = serialNumber || Math.floor(Math.random() * 100000) + '';
   var now = Date.now();
   // compatible with apple's updated cert policy: https://support.apple.com/en-us/HT210176
   cert.validity.notBefore = new Date(now - 24 * 60 * 60 * 1000); // 1 day before
   cert.validity.notAfter = new Date(now + 824 * 24 * 60 * 60 * 1000); // 824 days after
   return {
-    keys,
-    cert
+    keys: keys,
+    cert: cert
   };
 }
 
 function generateRootCA(commonName) {
-  const keysAndCert = getKeysAndCert();
-  const keys = keysAndCert.keys;
-  const cert = keysAndCert.cert;
+  var keysAndCert = getKeysAndCert();
+  var keys = keysAndCert.keys;
+  var cert = keysAndCert.cert;
 
   commonName = commonName || 'CertManager';
 
-  const attrs = defaultAttrs.concat([
-    {
-      name: 'commonName',
-      value: commonName
-    }
-  ]);
+  var attrs = defaultAttrs.concat([{
+    name: 'commonName',
+    value: commonName
+  }]);
   cert.setSubject(attrs);
   cert.setIssuer(attrs);
-  cert.setExtensions([
-    { name: 'basicConstraints', cA: true },
+  cert.setExtensions([{ name: 'basicConstraints', cA: true }]
   // { name: 'keyUsage', keyCertSign: true, digitalSignature: true, nonRepudiation: true, keyEncipherment: true, dataEncipherment: true },
   // { name: 'extKeyUsage', serverAuth: true, clientAuth: true, codeSigning: true, emailProtection: true, timeStamping: true },
   // { name: 'nsCertType', client: true, server: true, email: true, objsign: true, sslCA: true, emailCA: true, objCA: true },
   // { name: 'subjectKeyIdentifier' }
-  ]);
+  );
 
   cert.sign(keys.privateKey, forge.md.sha256.create());
 
@@ -78,27 +72,25 @@ function generateRootCA(commonName) {
 
 function generateCertsForHostname(domain, rootCAConfig) {
   // generate a serialNumber for domain
-  const md = forge.md.md5.create();
+  var md = forge.md.md5.create();
   md.update(domain);
 
-  const keysAndCert = getKeysAndCert(md.digest().toHex());
-  const keys = keysAndCert.keys;
-  const cert = keysAndCert.cert;
+  var keysAndCert = getKeysAndCert(md.digest().toHex());
+  var keys = keysAndCert.keys;
+  var cert = keysAndCert.cert;
 
-  const caCert = forge.pki.certificateFromPem(rootCAConfig.cert);
-  const caKey = forge.pki.privateKeyFromPem(rootCAConfig.key);
+  var caCert = forge.pki.certificateFromPem(rootCAConfig.cert);
+  var caKey = forge.pki.privateKeyFromPem(rootCAConfig.key);
 
   // issuer from CA
   cert.setIssuer(caCert.subject.attributes);
 
-  const attrs = defaultAttrs.concat([
-    {
-      name: 'commonName',
-      value: domain
-    }
-  ]);
+  var attrs = defaultAttrs.concat([{
+    name: 'commonName',
+    value: domain
+  }]);
 
-  const extensions = [
+  var extensions = [
     { name: 'basicConstraints', cA: false },
     getExtensionSAN(domain),
     { name: 'keyUsage', keyCertSign: false, digitalSignature: true, nonRepudiation: false, keyEncipherment: true, dataEncipherment: true },
